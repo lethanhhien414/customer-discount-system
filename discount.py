38d96cf91598e78a65f4bcb29a0b979abd6f2139
lethanhhien414 <lethanhhien414@gmail.com>
2026-06-11 17:00:40 +0700

    Implement loyalty customer discount feature

--- discount.py
+++ discount.py
@@ -1,4 +1,8 @@
-def calculate_discount(total_past_spending, current_order_value):
-    if total_past_spending >= 50000000:
-        return 0.1  # Giảm 10%
-    return 0.0
+def calculate_discount(previous_total, current_order):
+    if previous_total >= 50000000:
+        return 0.1
+
+    if previous_total + current_order >= 50000000:
+        return 0.1
+
+    return 0
--- test_discount.py
+++ test_discount.py
@@ -1,10 +1,10 @@
 from discount import calculate_discount
 
-def test_tc01():
-    assert calculate_discount(60000000, 20000000) == 0.1
+def test_vip_customer():
+    assert calculate_discount(60000000, 2000000) == 0.1
 
-def test_tc02():
-    assert calculate_discount(30000000, 20000000) == 0.0
+def test_normal_customer():
+    assert calculate_discount(30000000, 2000000) == 0
 
-def test_tc03():
-    assert calculate_discount(49000000, 20000000) == 0.1
+def test_reach_threshold_customer():
+    assert calculate_discount(49000000, 2000000) == 0.1
